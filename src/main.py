bbec2fce780b55ac60f7dd17bdf6f8ec2855738e
Qiu Qin <qiu.qin@oracle.com>
2021-08-06 22:30:00 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,3 +4,13 @@ from my_module import my_function_in_module
 print("This is the main script.")
 my_function_in_module()
 utils.my_function_in_package()
+
+def entry_function(*args, **kwargs):
+    print("This is the entry function.")
+    print("Arguments:")
+    print(args)
+    for k, v in kwargs.items():
+        print(f"{k}={v}")
+    my_function_in_module()
+    utils.my_function_in_package()
+
