994fe50fc15af22d34b1d9aedd0d6f943b222d41
Qiu Qin <qiu.qin@oracle.com>
2021-08-08 09:35:35 -0400

    Update main.py to write output.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
+import datetime
+import os
+
 from my_package import utils
 from my_module import my_function_in_module
+
+
+def write_output():
+    if "OUTPUT_DIR" in os.environ:
+        filename = datetime.datetime.now().strftime("%Y%m%d_%H%M%s.txt")
+        with open(os.path.join(os.environ.get("OUTPUT_DIR")), "w") as f:
+            f.write(filename)
 
 
 def entry_function(*args, **kwargs):
@@ -10,8 +20,10 @@ def entry_function(*args, **kwargs):
         print(f"{k}={v}")
     my_function_in_module()
     utils.my_function_in_package()
+    write_output()
 
 if __name__ == "__main__":
     print("This is the main script.")
     my_function_in_module()
     utils.my_function_in_package()
+    write_output()
