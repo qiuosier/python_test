8d6d1a86ecbc4a0ecadeb837e0c42404467adcab
Qiu Qin <qiu.qin@oracle.com>
2023-02-10 12:06:02 -0500

    Update main.py and test_notebook.ipynb

--- src/main.py
+++ src/main.py
@@ -6,14 +6,18 @@ from my_package import utils
 from my_module import my_function_in_module
 
 
-def write_output():
-    output_dir = os.environ.get("OUTPUT_DIR", "./outputs")
-    output_dir = os.path.abspath(os.path.expanduser(output_dir))
+def write_output(filename=None):
+    if not filename:
+        output_dir = os.environ.get("OUTPUT_DIR", "./outputs")
+        output_dir = os.path.abspath(os.path.expanduser(output_dir))
+        filename = os.path.join(
+            output_dir,
+            datetime.datetime.now().strftime("%Y%m%d_%H%M%s.txt")
+        )
+    else:
+        output_dir = os.path.dirname(os.path.abspath(os.path.expanduser(filename)))
     os.makedirs(output_dir, exist_ok=True)
-    filename = os.path.join(
-        output_dir,
-        datetime.datetime.now().strftime("%Y%m%d_%H%M%s.txt")
-    )
+    
     print(f"Writing {filename}...")
     with open(filename, "w") as f:
         f.write(filename)
@@ -36,4 +40,4 @@ if __name__ == "__main__":
         print(sys.argv[1:])
     my_function_in_module()
     utils.my_function_in_package()
-    write_output()
+    write_output(os.environ.get("OUTPUT_FILENAME"))
